6f282c6ce7773cd48895690c498785b14f0780ad
Nour Dandan <nour.dandan@gmail.com>
2021-03-07 16:49:24 +0100

    switch to parallel async

--- Virtuplex/MainWindow.xaml.cs
+++ Virtuplex/MainWindow.xaml.cs
@@ -36,25 +36,67 @@ namespace Virtuplex
             try
             {
                 tbAlternativeInput.Text += "---- Starting Calculation Event  ---- \r\n";
-                CalculateEvent(tbFileInput.Text, string.Concat(tbFileOutput.Text,@"\",tbFileName.Text));
+                //dont await so we don't block the wpf guid
+                CalculateEvent(tbFileInput.Text, string.Concat(tbFileOutput.Text, @"\", tbFileName.Text));
                 tbAlternativeInput.Text += "---- In Proccess  ---- \r\n";
             }
 
-            catch(Exception ex)
+            //catch exceptions from the parallel async 
+            catch (AggregateException ex)
+            {
+                foreach (var innerEx in ex.InnerExceptions)
+                {
+                    tbOutputText.Text = $"Error has occured : {Environment.NewLine} {innerEx.Message}";
+                }
+            }
+
+            catch (Exception ex)
             {
                 tbOutputText.Text = $"Error has occured : {Environment.NewLine} {ex.Message}";
             }
         }
 
+        async Task CalculateEvent(string inputFile, string outputFile)
+        {
+            var fileReadResult = ReadFile(inputFile);
+            await fileReadResult;
+            //wait until reading is done ,alternative method would be yo yield result
+            var res = EvaluateTasks(fileReadResult.Result.ToList());
+            var sb = new StringBuilder();
+            while (res.Count > 0)
+            {
+                //write when buffer size reached
+                var size = res.Count > bufferSize ? bufferSize : res.Count;
+                var batch = await Task.WhenAll(res.GetRange(0, size));
+                batch.ToList().ForEach(x => sb.AppendLine(x));
+                await WriteFile(outputFile, sb.ToString());
+                res.RemoveRange(0, size);
+                sb.Clear();
+            }
+        }
+
+        //set up tasks in list 
+        List<Task<string>> EvaluateTasks(IEnumerable<string> lines)
+        {
+            var listOfTasks = new List<Task<string>>();
+            tbAlternativeInput.Text += "---- Eval Starting  ---- \r\n";
+            foreach (var line in lines)
+            {
+                if (!string.IsNullOrEmpty(line))
+                    listOfTasks.Add(helper.Evaluate(line));
+            }
+            return listOfTasks;
+        }
+
         async Task<string[]> ReadFile(string inputFile)
         {
-            using(var stream = File.Open(inputFile, FileMode.Open))
+            using (var stream = File.Open(inputFile, FileMode.Open))
             {
-                using(var sr =  new StreamReader(stream))
+                using (var sr = new StreamReader(stream))
                 {
                     tbAlternativeInput.Text += "---- File reading started ---- \r\n";
-                    await Task.Delay(1000);
-                    var fileText =  sr.ReadToEndAsync();
+                    //await Task.Delay(1000);
+                    var fileText = sr.ReadToEndAsync();
                     tbAlternativeInput.Text += "---- File reading In Processs  ---- \r\n";
                     await fileText;
                     tbAlternativeInput.Text += "---- File reading completed  ---- \r\n";
@@ -63,53 +105,18 @@ namespace Virtuplex
             }
         }
 
-        async Task WriteFile(string outputFile,List<string> arrayToWrite)
+        async Task WriteFile(string outputFile, string arrayToWrite)
         {
-            using (var stream = File.Open(outputFile, FileMode.Create, FileAccess.Write))
+            using (var stream = File.Open(outputFile, FileMode.Append, FileAccess.Write))
             {
                 using (var sw = new StreamWriter(stream))
                 {
                     tbAlternativeInput.Text += "---- File Writing started  ---- \r\n";
-                    arrayToWrite.ForEach(result => sw.WriteLineAsync(result));
+                    await sw.WriteAsync(arrayToWrite);
                     tbAlternativeInput.Text += "---- File Writing completed  ---- \r\n";
                 }
             }
         }
 
-        async Task CalculateEvent(string inputFile , string outputFile)
-        {
-            var sb = new StringBuilder();
-            sw.Reset();
-            sw.Start();
-            var inputLines = await ReadFile(inputFile);
-            tbAlternativeInput.Text += $"---- Read file in {sw.ElapsedMilliseconds}  ---- \r\n";
-            sw.Stop();
-            var count = 0;
-            var bufferData = new List<string>();
-            foreach ( var inputLine in  inputLines)
-            {
-                if (!string.IsNullOrEmpty(inputLine))
-                {
-                    count++;
-                    tbAlternativeInput.Text += $"---- Currently Evaluating line {count}  ---- \r\n";
-                    sw.Reset();
-                    sw.Start();
-                    var evaluationResult = helper.Evaluate(inputLine);
-                    tbAlternativeInput.Text += "---- long evaluation in process  ---- \r\n";
-                    if (bufferData.Count() == bufferSize)
-                    {
-                        await WriteFile(outputFile, bufferData);
-                        bufferData.Clear();
-                    }
-                    var result = $"{await evaluationResult}";
-                    tbAlternativeInput.Text += $"---- Finished Evaluating {count} ,  finished in {sw.ElapsedMilliseconds}  ---- \r\n";
-                    tbOutputText.Text += $"{result} -line {count} \r\n";
-                    bufferData.Add(result);
-                }
-            }
-            await WriteFile(outputFile, bufferData);
-            bufferData.Clear();
-            tbAlternativeInput.Text += "---- Process Finished  ---- \r\n";
-        }
     }
 }
